80e94036524058f3db22c92fdd353aed58189cf1
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-09-27 06:30:17 +0530

    office : schem

--- Vayal/officer/models.py
+++ Vayal/officer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+schem_type=[('Janakeeyasoothranam','Janakeeyasoothranam'),
+            ('Department of agriculture','Department of agriculture')]
+
+class Schem(models.Model):
+    name=models.CharField(max_length=250)
+    type=models.CharField(max_length=50,choices=schem_type)
+    description=models.CharField(max_length=250)
+    criteria=models.CharField(max_length=250)
+    start_date=models.DateTimeField(auto_now_add=True)
+    end_date=models.DateField()
+
+    def __str__(self):
+        return self.name
