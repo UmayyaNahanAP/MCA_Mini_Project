cdc4b789e54e3287e07ab28fbf4e966f2503d1e7
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-09-27 06:18:54 +0530

    initial commit

--- Haritha_Mandiram/hm_user/urls.py
+++ Haritha_Mandiram/hm_user/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
 from .views import reg,schems,notification,home
-
 app_name='hm_user'
 
 urlpatterns = [
     path('home/',home.index,name='home'),
     path('sign_up/',reg.index,name='sign_up'),
-    path('login/',login,name='home'),
+    path('login/',reg.login,name='login'),
 
     path('schems/',schems.index,name='index'),
     path('schems/<int:id>/',schems.details,name='details'),
 
     path('notification/',notification.index, name='notifications'),
-    
+
 ]
--- Haritha_Mandiram/officer/admin.py
+++ Haritha_Mandiram/officer/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import HM_User,Schem,Notification
+from .models import HM_User,Schem,Notification,Complaint
 
 # Register your models here.
 
 admin.site.register(HM_User)
 admin.site.register(Schem)
 admin.site.register(Notification)
+admin.site.register(Complaint)
 # admin.site.register(SchemApplication)
