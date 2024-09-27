0ed6b75c780a6fe9ac010e8fc1046f7dd08eb9c1
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-09-27 06:31:39 +0530

    officer : schems

--- Vayal/officer/urls.py
+++ Vayal/officer/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from .views import schems
 
 app_name='officer'
 
 urlpatterns = [
+    path('schems/',schems.index,name='schems'),
+    path('schems/create/',schems.create,name='create_schem'),
+    path('schems/<int:id>/update/',schems.update,name='update_schem'),
+    path('schems/<int:id>/',schems.details,name='details_schem'),
+    path('schems/<int:id>/delete/',schems.delete,name='delete_schem'),
 ]
