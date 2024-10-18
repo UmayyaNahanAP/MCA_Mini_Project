0dce745c10880feab6120568ddffcf366ce424c4
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-10-18 21:45:35 +0530

    vayal user : registration

---
+++ Vayal/vayal_user/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from django.forms import EmailField, PasswordInput, CharField
+from .models import Vayal_User
+
+
+class VayalUserRegistrationForm(ModelForm):
+    email = EmailField()
+    password = CharField(widget=PasswordInput())
+    class Meta:
+        model=Vayal_User
+        fields=['name','dob','genter','phone_number','aadhar_number','cast',
+                'house_name','place','village','pincode','land_ownership','photo']
