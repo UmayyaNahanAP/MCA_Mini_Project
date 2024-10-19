5607e7b16ada55fb52f59cd15fde9633bea49c28
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-10-19 19:29:15 +0530

    user : scheme

--- Vayal/vayal_user/urls.py
+++ Vayal/vayal_user/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import reg,notification,complaint
+from .views import schemes,reg,notification,complaint
 
 
 app_name='vayal_user'
 
 urlpatterns = [
     path('register/',reg.index,name='register'),
+
+    path('schemes/',schemes.index,name='schemes'),
+    path('schemes/<int:id>/',schemes.details,name='details_schemes'),
+
 
     path('notification/',notification.index, name='notifications'),
     path('notification/<int:id>/',notification.details,name='details_notification'),
