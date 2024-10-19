0a8cdfd43e5bae65dd0d0526cb4d27f5339841a9
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-10-19 06:25:30 +0530

    new

--- Vayal/vayal_user/forms.py
+++ Vayal/vayal_user/forms.py
@@ -9,10 +9,16 @@ class VayalUserRegistrationForm(ModelForm):
     password = CharField(widget=PasswordInput())
     class Meta:
         model=Vayal_User
-        fields=['name','dob','genter','phone_number','aadhar_number','cast',
-                'house_name','place','village','panchayat_muncipality_corporation',
-                'district','pincode','house_number','ward_number','land_ownership',
-                'total_land_area','land_survay_number','photo']
+        fields=['name','dob','gender','phone_number','aadhar_number','cast',
+                'house_name','place','village','pincode','land_ownership','photo']
+
+    
+    # class Meta:
+    #     model=Vayal_User
+    #     fields=['name','dob','genter','phone_number','aadhar_number','cast',
+    #             'house_name','place','village','panchayat_muncipality_corporation',
+    #             'district','pincode','house_number','ward_number','land_ownership',
+    #             'total_land_area','land_survay_number','photo']
 
 
 
--- Vayal/vayal_user/models.py
+++ Vayal/vayal_user/models.py
@@ -20,24 +20,46 @@ class Vayal_User(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
     name=models.CharField(max_length=225)
     dob=models.DateField()
-    genter= models.CharField(max_length=20, choices=gender)
+    gender= models.CharField(max_length=20, choices=gender)
     phone_number = models.CharField(max_length=10)
     aadhar_number= models.CharField(max_length=12)
     cast=models.CharField(max_length=20, choices=cast)
     house_name=models.CharField(max_length=225)
     place=models.CharField(max_length=225)
     village=models.CharField(max_length=225)
-    panchayat_muncipality_corporation=models.CharField(max_length=225)
-    district=models.CharField(max_length=225,default="Kozhikode")
     pincode = models.CharField(max_length=6)
-    house_number= models.CharField(max_length=10)
-    ward_number=models.CharField(max_length=3)
+    muncipality=models.CharField(max_length=225,default="Kozhikode")
+    district=models.CharField(max_length=225,default="Kozhikode")
     land_ownership=models.CharField(max_length=20, choices=land)
-    total_land_area=models.CharField(max_length=225)
-    land_survay_number=models.CharField(max_length=225)
     photo=models.ImageField(upload_to='documents/photo/')
     def __str__(self):
         return self.name
+    
+
+
+
+# class Vayal_User(models.Model):
+#     account = models.OneToOneField(User, on_delete=models.CASCADE)
+#     name=models.CharField(max_length=225)
+#     dob=models.DateField()
+#     genter= models.CharField(max_length=20, choices=gender)
+#     phone_number = models.CharField(max_length=10)
+#     aadhar_number= models.CharField(max_length=12)
+#     cast=models.CharField(max_length=20, choices=cast)
+#     house_name=models.CharField(max_length=225)
+#     place=models.CharField(max_length=225)
+#     village=models.CharField(max_length=225)
+#     panchayat_muncipality_corporation=models.CharField(max_length=225)
+#     district=models.CharField(max_length=225,default="Kozhikode")
+#     pincode = models.CharField(max_length=6)
+#     house_number= models.CharField(max_length=10)
+#     ward_number=models.CharField(max_length=3)
+#     land_ownership=models.CharField(max_length=20, choices=land)
+#     total_land_area=models.CharField(max_length=225)
+#     land_survay_number=models.CharField(max_length=225)
+#     photo=models.ImageField(upload_to='documents/photo/')
+#     def __str__(self):
+#         return self.name
     
 
 # class Vayal_User(models.Model):
