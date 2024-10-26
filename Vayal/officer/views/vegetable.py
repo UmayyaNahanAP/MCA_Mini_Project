2ab003524086899bb23d72eac8d4e078b0c2faf1
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-10-26 22:27:18 +0530

    vegetable permission

--- Vayal/officer/views/vegetable.py
+++ Vayal/officer/views/vegetable.py
@@ -5,15 +5,20 @@ from officer.forms import VegetablePermissionForm,Vegetable
 
 
 def index(request):
+    status = request.GET.get('status')
     vegetable_permissions= VegetablePermission.objects.all().order_by('-id')
-    return render(request,'officer/vegetable/index.html', {'vegetable_permissions':  vegetable_permissions})
-
+    if status:
+        vegetable_permissions = vegetable_permissions.filter(status=status)
+    context = {
+        'status': status,
+        'vegetable_permissions': vegetable_permissions,
+    }
+    return render(request,'officer/vegetable/index.html',context)
 
 
 def user_details(request,application_id,vayal_user_id):
-    vegetable_permission = VegetablePermission.objects.get(id=application_id,vayal_user_id=vayal_user_id)
+    vegetable_permission = VegetablePermission.objects.get(id=application_id, vayal_user_id=vayal_user_id)
     return render(request, 'officer/vegetable/user_details.html', {'vegetable_permission': vegetable_permission})
-
 
 # def user_details(request,application_id,vayal_user_id):
 #     scheme_application = SchemeApplication.objects.get(id=application_id, vayal_user_id=vayal_user_id)
