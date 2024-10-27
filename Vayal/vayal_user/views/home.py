ce8b77995707310e60fc788cea055175045fce12
UmayyaNahanAP <umayyanahanap2002@gmail.com>
2024-10-28 00:30:14 +0530

    all..

--- Vayal/vayal_user/views/home.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from vayal_user.models import Vayal_User
-
-
-def index(request):
-    vayal_user = Vayal_User.objects.get(account=request.user)
-    return render(request, 'vayal_user/home/index.html',{'vayal_user': vayal_user})
-
